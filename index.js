93eec11d2c444e7b3aa96346c6ab97d726f71bfb
Jacob12138xieyuan <xiey0017@ntu.edu.sg>
2021-11-30 22:07:26 +0800

    express works

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.use(express.static('Fund Manager'));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/Fund Manager/index.html'));
+});
 
 app.listen(4200, () => {
 	console.log('Application started and Listening on port 4200');
-});
-
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/Fund Manager/index.html');
 });
